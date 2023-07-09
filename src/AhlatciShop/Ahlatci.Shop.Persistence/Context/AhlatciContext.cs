8650733289727fbb2e83adaa2973e21084f34f4a
eren agir <agir_eren@hotmail.com>
2023-07-09 14:04:47 +0300

    context oluşturuldu

---
+++ src/AhlatciShop/Ahlatci.Shop.Persistence/Context/AhlatciContext.cs
@@ -0,0 +1,69 @@
+﻿using Ahlatci.Shop.Domain.Common;
+using Ahlatci.Shop.Domain.Entities;
+using Ahlatci.Shop.Persistence.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ahlatci.Shop.Persistence.Context
+{
+    public class AhlatciContext : DbContext
+    {
+        public AhlatciContext(DbContextOptions<AhlatciContext> options) : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AccountMapping());
+            modelBuilder.ApplyConfiguration(new AddressMapping());
+            modelBuilder.ApplyConfiguration(new CategoryMapping());
+            modelBuilder.ApplyConfiguration(new CityMapping());
+            modelBuilder.ApplyConfiguration(new CommentMapping());
+            modelBuilder.ApplyConfiguration(new CustomerMapping());
+            modelBuilder.ApplyConfiguration(new OrderMapping());
+            modelBuilder.ApplyConfiguration(new OrderDetailMapping());
+            modelBuilder.ApplyConfiguration(new ProductMapping());
+            modelBuilder.ApplyConfiguration(new ProductImageMapping());
+
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>().ToList())
+            {
+                switch (entry.State)
+                {
+                    //update
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedDate = DateTime.Now;
+                        entry.Entity.modifiedBy = "admin";
+                        break;
+                    //insert
+                    case EntityState.Added:
+                        entry.Entity.CreateDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "admin";
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
